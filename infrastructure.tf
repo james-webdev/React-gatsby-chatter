e9fbfbf9c0edcfe35dc358d0c0d2621744f91493
gendallstudio <support@gendall.co.uk>
2019-10-12 11:03:11 +0000

    Initial commit

---
+++ infrastructure.tf
@@ -0,0 +1,68 @@
+terraform {
+  backend "gcs" {}
+}
+
+variable "project" {}
+variable "env" {}
+variable "output" {}
+
+# providers #
+
+provider "digitalocean" {
+  version = "~> 1.6"
+}
+
+provider "cloudflare" {
+  version = "~> 1.17"
+}
+
+provider "acme" {
+  version = "~> 1.4"
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+provider "tls" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.3"
+}
+
+# servers #
+
+module "manager" {
+  source  = "/etc/terraform/modules/digitalocean_droplet"
+  project = "chatter-web"
+  env     = var.env
+  group   = "chatter"
+  size    = "s-1vcpu-1gb"
+  role    = "manager"
+}
+
+# network #
+
+module "dns" {
+  source  = "/etc/terraform/modules/cloudflare_acme_record"
+  zone    = "gendall.io"
+  env     = var.env
+  records = {
+    "chatter-web" = module.manager.ip
+  }
+}
+
+# output #
+
+module "inventory" {
+  source  = "/etc/terraform/modules/local_file_inventory"
+  groups  = {     
+    manager  = module.manager.ip
+  }
+  tls_key  = module.dns.tls_key
+  tls_cert = module.dns.tls_cert
+  target   = var.output
+}
