4f133d4121288b6b97394bfd471d986fc3f8f1d8
Ed Wilson <ed@gendall.co.uk>
2019-11-08 21:35:53 +0000

    trying to get the second animation timings right...

--- src/components/backgroundsec2.js
+++ src/components/backgroundsec2.js
@@ -7,25 +7,6 @@ import styled from "styled-components"
 
 const AnimationWrapper = styled.div`
   position: relative;
-  .titleWrapper {
-    position: absolute;
-    z-index: 2;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    top: 50%;
-    transform: translateY(-50%);
-    ${MEDIA.PHONE`
-        
-    `}
-    ${MEDIA.TABLET`
-        
-    `}
-    h2 {
-      margin-left: 0;
-      max-width: 80%;
-    }
-  }
   .middle-animation-wrapper {
     ${MEDIA.PHONE`
       transform: scale(2);
@@ -33,15 +14,77 @@ const AnimationWrapper = styled.div`
   `}
   }
 `
+const TitleWrapper = styled.div`
+  position: absolute;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 50%;
+  transform: translate(-50%);
+  max-width: 100%;
+  left: 50%;
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    max-width: 100%;
+    li {
+      font-family: "Windsor", sans-serif;
+      font-size: 5.04rem;
+      line-height: 1.1;
+      color: white;
+      list-style: none;
+      animation: wordFading 3s infinite;
+      transition-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+      margin: 0 20px;
+      position: absolute;
+      left: 50%;
+      width: 100vw;
+      text-align: center;
+      transform: translate(-50%);
+      background-color: #453b63;
+      &.first {
+        animation-delay: 2s;
+      }
+      &.second {
+        animation-delay: 8s;
+      }
+      &.third {
+        animation-delay: 12s;
+      }
+    }
+  }
+  ${MEDIA.PHONE`
+        
+    `}
+  ${MEDIA.TABLET`
+        
+    `}
+  @keyframes wordFading {
+    0% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`
 
 const BackgroundSec2 = () => (
   <AnimationWrapper>
     <MiddleAnimation />
-    <div className="titleWrapper">
-      <Title as="h2" color="white">
-        We innovate where intelligent voice technology and human behaviour meet.
-      </Title>
-    </div>
+    <TitleWrapper>
+      <ul>
+        <li className="first">We innovate where</li>
+        <li className="second">intelligent voice technology</li>
+        <li className="third">and human behaviour meet.</li>
+      </ul>
+    </TitleWrapper>
   </AnimationWrapper>
 )
 
